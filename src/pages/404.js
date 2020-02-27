68934ba3fa36065ea1cc310db31a67d8821afa6f
Hiroki1112 <pystdy@yahoo.co.jp>
2020-02-27 14:05:19 +0900

    2-27

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ const NotFoundPage = () => (
     <div>
       <h1>NOT FOUND 404</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      <img src="/img/for404.png"/>
+      <img src="/img/for404.png" alt="404"/>
     </div>
   </Layout>
 )
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
-import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
 
 export const IndexPageTemplate = ({
@@ -80,7 +79,7 @@ export const IndexPageTemplate = ({
                 </div>
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
+                    Latest Posts
                   </h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
